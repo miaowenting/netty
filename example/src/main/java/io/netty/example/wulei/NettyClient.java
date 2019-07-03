fddab4a77809745063c6f965a91fd83986e49252
wulei <wulei.wl@dtwave-inc.com>
2019-07-03 20:31:41 +0800

    netty-server junit test

---
+++ example/src/main/java/io/netty/example/wulei/NettyClient.java
@@ -0,0 +1,58 @@
+package io.netty.example.wulei;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class NettyClient {
+	
+	public static void main(String[] args) {
+		
+		EventLoopGroup group = new NioEventLoopGroup(1);
+		
+		try{
+			Bootstrap boostrap = new Bootstrap();
+			boostrap.channel(NioSocketChannel.class);
+			boostrap.group(group);
+			boostrap.option(ChannelOption.SO_KEEPALIVE, true)
+			             .handler(new ChannelInitializer<NioSocketChannel>() {
+							@Override
+							protected void initChannel(NioSocketChannel ch)
+									throws Exception {
+								ch.pipeline().addLast(new DelimiterBasedFrameDecoder
+										(Integer.MAX_VALUE,Delimiters.lineDelimiter()[0]));
+								ch.pipeline().addLast(new ClientHandler());
+								ch.pipeline().addLast(new StringEncoder());
+								
+							}
+						});
+			
+			ChannelFuture future = boostrap.connect("localhost", 8080).sync();
+			String person = "张三\r\n";
+			
+//			ByteBuf buf = PooledByteBufAllocator.DEFAULT.buffer();
+//			buf.writeBytes(person.getBytes(Charset.defaultCharset()));
+			future.channel().writeAndFlush(person);
+//			future.channel().writeAndFlush(Delimiters.lineDelimiter()[0]);
+			
+			
+			future.channel().closeFuture().sync();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			group.shutdownGracefully();
+		}
+		
+		
+		
+	}
+
+}
---
+++ example/src/main/java/io/netty/example/wulei/NettyServer.java
@@ -0,0 +1,49 @@
+package io.netty.example.wulei;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+
+public class NettyServer {
+	
+	public static void main(String[] args) {
+		EventLoopGroup parentGroup = new NioEventLoopGroup(1);
+		EventLoopGroup childGroup = new NioEventLoopGroup();
+		try {
+			ServerBootstrap serverBootstrap = new ServerBootstrap();
+			serverBootstrap.group(parentGroup, childGroup);
+			serverBootstrap.channel(NioServerSocketChannel.class);
+			serverBootstrap.option(ChannelOption.SO_BACKLOG, 128)
+			                         .option(ChannelOption.SO_KEEPALIVE, true)
+			                         .childHandler(new ChannelInitializer<SocketChannel>() {
+										@Override
+										protected void initChannel(SocketChannel ch)
+												throws Exception {
+											ch.pipeline().addLast(new DelimiterBasedFrameDecoder(Integer.MAX_VALUE,Delimiters.lineDelimiter()[0]));
+											ch.pipeline().addLast(new SimpleHandler());
+										}
+									});
+			
+			ChannelFuture future = serverBootstrap.bind(8080).sync();
+			future.channel().closeFuture().sync();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally{
+			parentGroup.shutdownGracefully();
+			childGroup.shutdownGracefully();
+		}
+		
+		
+		
+		
+	}
+
+}
